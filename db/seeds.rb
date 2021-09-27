16eda8aa1ea6a64d61f7eac222ed5fe2e9f2a23c
webgem-jpl <julienplefebvre@gmail.com>
2021-09-27 01:11:34 -0400

    option type seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,51 @@
 
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
+
+
+# Option Type Color
+color   = Spree::OptionType.create!(name:"Color", presentation: "Color")
+    
+red     = Spree::OptionValue.create!(
+            name: "Red",
+            presentation: "red",
+            option_type_id: color.id)
+
+black   = Spree::OptionValue.create!(
+            name: "Black",
+            presentation: "black",
+            option_type_id: color.id)
+
+pink     = Spree::OptionValue.create!(
+            name: "Pink",
+            presentation: "pink",  
+            option_type_id: color.id)
+
+# Option Type Size
+size    = Spree::OptionType.create!(name:"Size", presentation: "Size")
+
+small   = Spree::OptionValue.create!(
+            name: "Small",
+            presentation: "small",
+            option_type_id: size.id)
+
+medium  = Spree::OptionValue.create!(
+            name: "Medium",
+            presentation: "medium",
+            option_type_id: size.id)
+
+large   = Spree::OptionValue.create!(
+            name: "Large",
+            presentation: "large",
+            option_type_id: size.id)
+
+# Prototype
+shirt   = Spree::Prototype.create!(
+            name: 'Shirt',
+            presentation: 'shirt',
+            sale_channel:  true
+        )
+
+shirt.option_types = [color, size]
+shirt.save!
+
