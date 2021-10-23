9a3d75ee120486d9154b6d7ea7e6ffc0c1765a03
webgem-jpl <julienplefebvre@gmail.com>
2021-10-22 21:59:55 -0400

    update

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,11 @@
-Resque.redis = ENV.fetch('REDIS_URL', 'localhost:6379')
+Resque.redis = ENV.fetch('REDIS_URL', 'localhost:6379/spree-store')
 Resque.logger.level = Logger::DEBUG
+
+Resque.before_fork = Proc.new { 
+    ActiveRecord::Base.establish_connection
+
+    # Create a new buffered logger
+    Resque.logger = Logger.new(STDOUT)
+    Resque.logger.level = Logger::INFO
+    Resque.logger.debug "Resque Logger Initialized!"
+  }
