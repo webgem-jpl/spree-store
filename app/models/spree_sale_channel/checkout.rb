f284e0996f724344f979085b37bc302b91c26a4c
webgem-jpl <julienplefebvre@gmail.com>
2021-10-22 21:59:43 -0400

    moved to spree_sale_channel

--- app/models/spree_sale_channel/checkout.rb
+++
@@ -1,5 +0,0 @@
-module SpreeSaleChannel
-    class Checkout < ApplicationRecord
-        belongs_to :order, class_name: "Spree::Order"
-    end
-end
--- app/models/spree_sale_channel/shop.rb
+++
@@ -1,5 +0,0 @@
-module SpreeSaleChannel
-    class Shop < ApplicationRecord
-        has_many :products, class_name: "Spree::Product"
-    end
-end
